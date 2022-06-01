b278fb29a4e510b1db35e4c2fca6f408b68f61de
Claire Morgan-Sanders <crmo2444@gmail.com>
2022-06-01 14:53:29 -0500

    added total quantity + prices to purchase history page

--- src/components/products/PurchaseList.js
+++ src/components/products/PurchaseList.js
@@ -8,6 +8,7 @@ export const PurchaseList = ({id, customerId}) => {
     const [usersList, setUserList] = useState([])
     const [loggedInUser, setCurrentUser] = useState([])
     const [products, setProducts] = useState([])
+    const [updatedPurchases, setQuantityPerProduct] = useState([])
 
     const localKandyUser = localStorage.getItem("kandy_user")
     const kandyUserObject = JSON.parse(localKandyUser)
@@ -41,27 +42,61 @@ export const PurchaseList = ({id, customerId}) => {
         },
         [purchases]
     )
-    
-    let counter = 0
 
+    useEffect(
+        () => {
+            const categorizedPurchases = new Map()
+
+            userPurchases.map(userPurchase => {
+                let object = JSON.stringify(userPurchase.product.price + ':' + userPurchase.product.name)
+                if (categorizedPurchases.has(object)) {
+                    categorizedPurchases.set(object, categorizedPurchases.get(object) + 1)
+                }
+                else {
+                    categorizedPurchases.set(object, 1)
+                }
+            })
+
+            const categorizedPurchasesArray = Array.from(categorizedPurchases, ([key, value]) => {
+                return {product: JSON.parse(key),
+                        quantity: value};
+              });
+
+            setQuantityPerProduct(categorizedPurchasesArray)
+
+        },
+        [userPurchases]
+    )
+
+    let counter = 0
+    let splitString
+    let firstHalf
+    let secondHalf
+    let totalTotal = 0
+    
     return <>
     <h2>Purchase History for {loggedInUser.name}</h2>
     <article className="products">
-    {  userPurchases.length !== 0 ? <> 
-            {userPurchases.map((userPurchase) => {
+    {  userPurchases.length !== 0 ? 
+            <>  
+            {updatedPurchases.map((userPurchase) => {
                 counter++
+                splitString = userPurchase.product.split(":")
+                firstHalf = splitString[0]
+                secondHalf = splitString[1]
+                totalTotal += (firstHalf * userPurchase.quantity)
                     return <section className="product" key={`product--${userPurchase.id}`}>
                         <>
                         <div>Order #{counter}</div>
-                        <div>Product: {userPurchase?.product?.name}</div>
+                        <div>Product: {secondHalf}</div>
                         <div>Quantity: {userPurchase.quantity}</div>
-                        <div>Total Price: ${userPurchase?.product?.price}</div>
-                        <div>Date Placed: {userPurchase.date}</div>
+                        <div>Total Price: {(firstHalf * userPurchase.quantity).toLocaleString('en-US', {style:'currency', currency:'USD'})}</div>
                         </> 
                     </section>
              })} </> : <div>No orders placed.</div>
     }
-
     </article>
+
+    <h3>Total Price of Purchases: {totalTotal.toLocaleString('en-US', {style:'currency', currency:'USD'})}</h3>
     </>
 }
